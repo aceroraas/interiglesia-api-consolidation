9ad59dbc4a52f7e9c7f5ccb6fcde9f93bc29d2d9
RAASDEV <acerocol67@gmail.com>
2024-08-29 00:00:23 -0400

    ajusted de ficheros

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
 import express from 'express';
-import { PrismaClient } from '@prisma/client';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 import cors from 'cors';
 import getPort from 'get-port';
+import { generatePortRange } from './services/generatePortRange.js';
+import { logger } from './middleware/logger.js';
 
-const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(logger);
 
-const JWT_SECRET = process.env.JWT_SECRET;
-const generatePortRange = (start, end) => {
-   return Array.from({ length: end - start + 1 }, (_, i) => start + i);
-};
-// Middleware para registrar solicitudes
-app.use((req, res, next) => {
-   if (req.method === 'POST' || req.method === 'GET') {
-      console.log(JSON.stringify({
-         method: req.method,
-         url: req.url,
-         path: req.path,
-         params: req.params,
-         query: req.query,
-         body: req.body
-      }, null, 2));
-   }
-   next();
-});
 
 (async () => {
    const portRange = generatePortRange(3000, 3100);
---
+++ services/jwtsecret.js
@@ -0,0 +1,1 @@
+export const jwtsecret = () => process.env.JWT_SECRET;
